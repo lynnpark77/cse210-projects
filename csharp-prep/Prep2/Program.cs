353eef5fb802f71b0eb976833a015ee62a8a7276
lynnpark77 <raphaie@gmail.com>
2024-11-09 19:23:48 +0900

    Create grading program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        string userInput = Console.ReadLine();
+        int x = int.Parse(userInput);
+
+        if (x>=90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (x>=80 && x<90)
+        {
+            Console.WriteLine("B");
+        }
+        else if (x>=70 && x<80)
+        {
+            Console.WriteLine("C");
+        }
+        else if (x>=60 && x<70)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+            if (x>=70)
+            {
+                Console.WriteLine("Congratualations! You passed the class");
+            }
+            else
+            {
+                Console.WriteLine("You didn't pass. Try again");
+            }
     }
 }
