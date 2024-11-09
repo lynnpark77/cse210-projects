321750cb1c39054421aee1c2a013342f0f804756
lynnpark77 <raphaie@gmail.com>
2024-11-09 21:53:45 +0900

    core requirement 1 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?  ");
+        string number = Console.ReadLine();
+        int x = int.Parse(number);
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int y = int.Parse(guess);
+
+        if (x > y)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (x < y)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
