c7f0629898ba66893670b10c46c8809fe47725f1
lynnpark77 <raphaie@gmail.com>
2024-11-09 22:13:08 +0900

    core requirement 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,29 +1,37 @@
 using System;
 using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number?  ");
-        string number = Console.ReadLine();
-        int x = int.Parse(number);
+        string response = "You guessed it.";
+        do
+        {
+            Console.Write("What is the magic number?  ");
+            string number = Console.ReadLine();
+            int x = int.Parse(number);
 
-        Console.Write("What is your guess? ");
-        string guess = Console.ReadLine();
-        int y = int.Parse(guess);
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            int y = int.Parse(guess);
+            
+            if (x > y)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (x < y)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
 
-        if (x > y)
-        {
-            Console.WriteLine("Higher");
-        }
-        else if (x < y)
-        {
-            Console.WriteLine("Lower");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
-        }
+        } while (response == "You guessed it");
+
+       
     }
 }
