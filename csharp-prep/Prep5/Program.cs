876802381bc9be4104bc15a41099f2aaa3a19616
lynnpark77 <raphaie@gmail.com>
2024-11-11 12:01:47 +0900

    core requirement completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Globalization;
 
-class Program
+class ProgramteN
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            
+            return name;
+
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            return number;
+        }
+        static int SquareNumber(int number)
+        {
+            int square = number^2;
+            return square;
+        }
+        static void DisplayResult(string name,int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+
     }
 }
