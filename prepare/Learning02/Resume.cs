5df31a6ae1eadde721e3d0c6329cc880e7c68146
lynnpark77 <raphaie@gmail.com>
2024-11-14 09:21:42 +0900

    modify the resume file

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -3,9 +3,6 @@ public class Resume
     public string _name ="";
     public List<Job> _jobs = new List<Job>();
 
-    public Resume()
-    {    
-    }
     public void Display()
     {
         Console.WriteLine($"Name: {_name}");
