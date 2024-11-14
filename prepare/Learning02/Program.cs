5afd14cd46778f50390db66e20d1acf98e41c7b8
lynnpark77 <raphaie@gmail.com>
2024-11-14 09:41:35 +0900

    erase redundant info

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,14 +9,12 @@ class Program
         job1._company = "Microsoft";
         job1._startYear = 2019;
         job1._endYear = 2022;
-        job1.DisplayJobDetails();
 
         Job job2 = new Job();
         job2._jobTitle = "Manager";
         job2. _company = "Apple";
         job2._startYear = 2022;
         job2._endYear = 2023;
-        job2.DisplayJobDetails();
 
         Resume myResume = new Resume();
         myResume._name = "Allison Rose";
