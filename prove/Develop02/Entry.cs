87ad12f8504b8e535dcc01e3dca753133796c040
lynnpark77 <raphaie@gmail.com>
2024-11-19 17:56:55 +0900

    complete creating stubs

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,17 @@
+public class Entry
+{
+    public string _date = "";
+    public string _promptText = "";
+    public string _entryText = "";
+    
+    public Entry ()
+    {
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {_date} - Prompt: {_promptText}");
+        Console.WriteLine($"{_entryText}");
+    }
+
+}
