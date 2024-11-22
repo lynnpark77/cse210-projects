04a05553cb6a566d803b60bdad0ea71b133fd006
lynnpark77 <raphaie@gmail.com>
2024-11-22 12:30:54 +0900

    Learning 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetTop()/f1.GetBottom());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetTop()/f2.GetBottom());
+
+        Fraction f3 = new Fraction(3,4);
+        f3.SetTop(3);
+        f3.SetBottom(4);
+        Console.WriteLine(f3.GetTop()/f3.GetBottom());
+        
     }
 }
